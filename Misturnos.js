da08ee139c261a1ae879789724183f08f11b80ff
Camila Las Heras <camila.westnet@gmail.com>
2023-04-17 20:59:44 +0000

    SetTimeOut

--- Misturnos.js
+++ Misturnos.js
@@ -1,7 +1,7 @@
 document.addEventListener(
     "DOMContentLoaded",
     function () {
-        //TRAIGO JSON GUARDADO
+        //TRAIGO JSON GUARDADO.
         let turnoSolicitado = localStorage.getItem("turnos");
         turnoSolicitado = JSON.parse(turnoSolicitado);
         console.log(turnoSolicitado);
@@ -12,19 +12,32 @@ document.addEventListener(
             var htmlObject = document.createElement("div"); // creo variable para pasar el string a HTML
             const infoTurno = ` <div class="card w-50">
         <div class="card-body">
-        <h5 class="card-title">Turno asignado con: ${element.nombre}</h5>
-            <p class="card-text">Nombre del paciente: ${element.nombreProfesional}</p>
-            <a href="#" class="btn btn-primary" id="cancelar">Cancelar turno</a>
+        <h5 class="card-title">Turno asignado con: ${element.nombreProfesional}</h5>
+            <p class="card-text">Nombre del paciente: ${element.nombre}</p>
+            <button type=button href="#" class="btn btn-primary cancelar">Cancelar turno</button>
             </div>
         </div>`;
             htmlObject.innerHTML = infoTurno; // Convierte el string al HTML.
             contenedorTurno.append(htmlObject); //abro el contenedor para agregar dentro el card que cree anteriormente
         });
+        //CONTROLADOR DE EVENTO
+        const botonesCancelar = document.querySelectorAll(".cancelar");
+        botonesCancelar.forEach((boton) => {
+            boton.addEventListener("click", cancelarturno);
+        });
     },
-    false,
-    function cancelarTurno() {
-        let cancelar=getElementById("cancelar-turno")
-        cancelar.addEventListener("click",confirmacionDeCa)
-    }
-    function cancelarTurno
+    false
 );
+
+function cancelarturno(event) {
+    const botonCancelar = event.target; //botón al que se hace click
+    const tarjetaTurno = botonCancelar.closest(".card"); // tarjeta que contiene el boton
+    const turnocancelado = document.createElement("div");
+    turnocancelado.innerHTML = `El turno fue cancelado con éxito`;
+    document.body.appendChild(turnocancelado);
+
+    setTimeout(() => {
+        document.body.removeChild(turnocancelado);
+        tarjetaTurno.remove(); // elimino la tarjeta del turno cancelado del DOM
+    }, 3000);
+}
